2a327c9639999af928d6a0a28136fbc0fb9ed626
kirkdavid <56079877+kirkdavid@users.noreply.github.com>
2019-10-03 00:56:30 +1000

    Add files via upload

---
+++ problem1.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background('lightgray');
+    fill('red');
+    ellipse(75, 75, 100);
+    fill('blue');
+    ellipse(150, 75, 100);
+}
---
+++ problem2.js
@@ -0,0 +1,34 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(51, 204, 255);
+    // chimney
+    rect(230, 85, 15, 40);
+    // roof
+    triangle(90, 140, 188, 80, 285, 140);
+    // building
+    rect(100, 140, 175, 150);
+    // window 
+    rect(120, 190, 60, 60);
+    line(150, 190, 150, 250);
+    line(120, 220, 180, 220);
+    // door
+    rect(205, 200, 50, 90);
+    // handle
+    ellipse(213, 245, 8, 8);
+    line(100, 200, 100,100);
+     		
+
+line(100, 100, 200, 100);
+
+line(200, 100, 200, 200);
+
+line(200, 200, 100, 200);
+
+line(100, 200, 100, 100);
+
+    
+    
+}
